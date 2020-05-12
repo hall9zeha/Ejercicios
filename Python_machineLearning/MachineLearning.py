8c26d39012bc660a30007a98c6463c32f0c331dc
BARRYZEHA <vmwaretars@gmail.com>
2020-05-12 15:09:46 -0500

    Todavia falta reconocimiento de patrones con tensorflow

--- Python_machineLearning/MachineLearning.py
+++ Python_machineLearning/MachineLearning.py
@@ -168,4 +168,107 @@ def desviacionEstandar():
     
     desviacion=varianzaLib()
     print("La desviacion estandar es: ",math.sqrt(desviacion))
+#Ejercicio secuencia de fibonacci
+def fibonacci():
+    position=int(input("Ingrese hasta qué número quiere generar la secuencia de fibonacci: "))
+    iterador=0
+    secuencia=1
+    anterior=0
     
+    while(iterador<position):
+        suma=anterior + secuencia
+        anterior=secuencia
+        secuencia=suma
+        iterador+=1
+        print(secuencia)
+def fibonacci2():
+    position=int(input("Posición: "))
+    i=0
+    a=0
+    r=1
+    while(i<position):
+        suma=r+a
+        a=r
+        r=suma
+        i+=1
+        print(r)
+#Ejercicio regresión lineal predecir el precio de una casa a partir de los metros cuadrados de esta
+def regresionLineal():
+    num=int(input("Número de casas: "))
+    pendiente=0
+    listaMetros=[]
+    listaPrecios=[]
+    resultado1=0
+    resultado2=0
+    resultado3=0
+    resultado4=0
+    resultado5=0
+    mediaPrecios=0
+    mediaMetros=0
+    intersecLineal=0
+    pronostico=0
+    i=0
+    indice=0
+    while(i<num):
+        mt=int(input("Metros cuadrados de la casa: "))
+        listaMetros.append(mt)
+        pr=int(input("Precio de la casa: "))
+        listaPrecios.append(pr)
+        i+=1
+    while(indice<num):
+        resultado1=resultado1 + (listaPrecios[indice]* listaMetros[indice])
+        resultado2=resultado2 + listaPrecios[indice]
+        resultado3=resultado3 + listaMetros[indice]
+        resultado4=resultado4 + math.pow(listaMetros[indice],2)
+        resultado5=math.pow(resultado3,2)
+        
+        indice+=1
+    mediaPrecios=stats.mean(listaPrecios)
+    mediaMetros=stats.mean(listaMetros)
+    pendiente=((num*resultado1)-((resultado2)*(resultado3)))/((num*(resultado4))-(resultado5))    
+    intersecLineal=mediaPrecios - ((pendiente)*(mediaMetros))
+    pronostico=intersecLineal + (pendiente* (num + 1))
+    print("El pronóstico para el siguiente dato es: ",pronostico)
+    
+#Abstracción simple de redes neuronales y compuertas lógicas
+#Compuerta lógica AND y ORD ejemplos en un solo método
+def compuertasLogicas():
+    i=0
+    lstA=[]
+    lstB=[]
+    lstOUT=[]
+    a=0
+    result=0
+    outPutBinario=0
+    peso=0
+    iterador=0
+    tipSalida=""
+      
+    print("ingrese solo unos y ceros de acuerdo al ejercicio: ")
+    print("Escoja el tipo de compuerta lógica  a probar ")
+    variable=int(input("presione 1 para AND y 2 para OR: "))
+    if(variable==1):
+        peso=-30
+    else:
+        peso=-10
+    while(a<4):
+        A=int(input("Ingrese el número binario para A: "))
+        lstA.append(A)
+        B=int(input("Ingrese el número binario para B: "))
+        lstB.append(B)
+        result=(peso + (lstA[a]*20)+ (lstB[a]*20))
+        if(result<0):
+            result=0
+        else:
+            result=1
+        lstOUT.append(result)
+        a+=1
+    if(peso==-30):
+        tipSalida="AND"
+    else:
+        tipSalida="OR"
+    print("INPUT A  -  B   OUTPUT", tipSalida)  
+    while(iterador<4):
+        print("     ",lstA[iterador],"   ",lstB[iterador],"    ", lstOUT[iterador])
+        iterador+=1
+      
